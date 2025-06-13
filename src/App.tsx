ccf6be63adac2d72cc2eecb391ab5ce08e0980ee
Anupama Neupane <anupaneupane1994@gmail.com>
2025-06-14 00:52:25 +0545

    added artworks section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import MyArtworks from './components/artworks';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <MyArtworks/>
       <Contact />
       <Footer />
     </main>
